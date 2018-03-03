540dcf8613c312e680c8f6dbdf8d0cb6ebd0c6ec
mpw8174 <36712150+mpw8174@users.noreply.github.com>
2018-03-03 17:14:41 -0500

    script finished

--- DNSenum.ps1
+++ DNSenum.ps1
@@ -1,38 +1,46 @@
-#Takes a list of DNS names and returns their IPs if they exist
-#Expects hosts.txt with one hostname per line
+<#
+DNSenum.ps1  3/3/18
+Author = Mark White
+Purpose: Takes a list of DNS names and returns their IPs if they exist
+
+Expects: file names hosts.txt in the same directory as this script with one hostname perline.  Hosts specified must be network reachable.
+
+Gives: file names host_IPs.txt in the same directory as this script. 
+    host_IPS.txt with have the following format for each line.  HOSTNAME: 192.168.1.1 OR HOSTNAME: UNREACHABLE
+#>
+
 
 $Output = @()
 
-hosts = Get-Content("hosts.txt")
 
-foreach ($host in $hosts){
-    $hostinfo = Test-Connection -Computername $host -ErrorAction SilentlyContinue
+$hosts = Get-Content $PSScriptRoot\hosts.txt
+
+foreach ($target in $hosts){
+    $hostinfo = Test-Connection -Computername $target -Count 1 -ErrorAction SilentlyContinue   #attempt to ping $target to get IP
     
-    if($hostinfo){
-        
-        $Host_properties = @{
-            Name = $hostinfo.Destination.ToString 
-            IP = $hostinfo.IPV4Address.IPAddressToString
-        }
-        
-        New-Object PsObject -Property $host_properties
+    if($hostinfo){                                          #If output of Test-Connection is NOT NULL...
         
         
-        $Output+= [System.Net.Dns]::GetHostAddresses($host)
+           
+            $IP = $hostinfo.IPV4Address.IPAddressToString   #grab IPv4 address from Test-Connection output object
+        
+            $Output += $target + ": " + $IP + "`r`n"        #append results to output string/array
     }
 
-     Else {    
-        $Host_properties = @{
-            Name = $hostinfo.Destination.ToString 
-            IP = 'Unreachable'
-        }
-
-        New-Object PsObject -Property $props
+     Else {                                                 #If output of Test-Connection IS NULL...
+        
+            $IP = 'UNREACHABLE'
+            
+            $Output += $target + ": " + $IP + "`r`n"        #append results to output string/array
+            
+        
     }
+}
 
-}|Sort ComputerName | Export-Csv .\DNS_enum_Report.csv -NoTypeInformation
+Write-Host $Output                                          
 
+Write-Host "Output saved in host_IPs.txt"
 
-
+Out-File -FilePath $PSScriptRoot\host_IPs.txt -InputObject $Output   
 
 
