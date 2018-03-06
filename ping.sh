fc04d80f626b071663d5e58ef55df7823f892131
Michael-Rhodes <mjr2563@rit.edu>
2018-03-05 22:44:29 -0500

    bash ping script

---
+++ ping.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# File: ping.sh
+#
+# Description: 
+
+
+function usage {
+	cat << EOF
+Usage: 	 ping.sh [host(s)]
+
+Valid host formats:
+		1. 1.1.1.1 - Single host
+		2. 1.1.1.1-1.1.255.254 - Host range
+		3. 1.1.1.1/21 - Entire subnet
+EOF
+	exit
+}
+
+##### get ip address(es) ####
+IPfile="/tmp/ipList"
+if [ -e listIP.py ]; then
+	python3 listIP.py $1 $IPfile
+	if [[ "$?" -ne "0" ]]; then
+		echo
+		usage
+	fi
+else
+	echo "Cannot find helper script listIP.py."
+	echo "Ensure listIP.py is in the same directory as scan.py."
+fi
+
+# ping addresses
+while read -r host; do	# for each IP address
+	ping -q -c 1 -W 2 $host >/dev/null 2>&1 
+	if [ "$?" -eq "0" ]; then # if ping returned successfully
+		echo $host is up. 
+	fi
+done < $IPfile
